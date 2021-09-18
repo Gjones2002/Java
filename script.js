e2f10148f3ac85e18ae5c8e69b3b2c5ee1903672
Gjones2002 <gjones68@mail.valenciacollege.edu>
2021-09-18 12:07:44 -0400

    main

---
+++ script.js
@@ -0,0 +1,14 @@
+
+var firstName = "Gunner"
+var lastName = "Jones"
+var age = "18"
+var msg;
+msg = "My name is Gunner Jones";
+alert(msg);
+msg = "I am 18 years old"
+alert(msg);   
+var str = "I am having a fun time in this class"
+console.log(str)
+
+
+
